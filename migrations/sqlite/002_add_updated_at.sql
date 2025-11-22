4eb8fc32039410878695a89fea26eff20de17c5c
Rachmat Aditiya <raditiya@me.com>
2025-11-23 06:33:59 +0700

    Fix SQLite migration syntax: split ALTER and UPDATE

--- migrations/sqlite/002_add_updated_at.sql
+++ migrations/sqlite/002_add_updated_at.sql
@@ -1,2 +1,3 @@
-ALTER TABLE users ADD COLUMN updated_at DATETIME DEFAULT CURRENT_TIMESTAMP;
-
+ALTER TABLE users ADD COLUMN updated_at DATETIME;
+-- separator
+UPDATE users SET updated_at = datetime('now') WHERE updated_at IS NULL;
